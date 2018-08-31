c69f977e20b359a66834cf28dbf912916168c3cd
chjxx <19616414@qq.com>
2018-08-31 14:25:46 +0800

    correct speed error

--- basic/54-57/app/script/player.js
+++ basic/54-57/app/script/player.js
@@ -15,13 +15,13 @@ export default class Player{
 }
 
 class FootballPlayer extends Player{
-  constructor({maxSpeed, energy, explose}){
+  constructor({maxSpeed, physical, explosive}){
     super();
-
-    this.radius = 10;
+    this.id = getRandom(1000, 9999);
+    this.radius = 10; // 显示的半径，10代表1米
     this.maxSpeed = maxSpeed || getRandom(1, 99);
-    this.energy = energy || getRandom(1, 99);
-    this.explose = explose || getRandom(1, 99);
+    this.physical = physical || getRandom(1, 99);
+    this.explosive = explosive || getRandom(1, 99);
   }
   enter(field){
     this.field = field;
@@ -46,6 +46,7 @@ class FootballPlayer extends Player{
     this.ctx.beginPath();
     this.ctx.moveTo(x, y);
     this.ctx.arc(x, y, this.radius, 0, Math.PI * 2);
+    // this.ctx.closePath();
     this.ctx.fill();
 
     this.ctx.restore();
@@ -55,45 +56,45 @@ class FootballPlayer extends Player{
   runTo(destinationX, destinationY){
     let self = this;
     let startTime = new Date();
-    let currentSpeed = self.calculateMeter(1);
-    let speedTime = self.calculateSpeedTime(self.explose);
-    let keepTime = self.calculateKeepTime(self.energy);
-    let xDirection = destinationX - self.startX;
-    let yDirection = destinationY - self.startY;
-    let xAbsDirection = Math.abs(xDirection);
-    let yAbsDirection = Math.abs(yDirection);
-    let angle = Math.atan2(yDirection, xDirection);
-    let max = false;
+    let currentSpeed = 1;
+    let speedUpTime = self.calculateSpeedUpTime(self.explosive); //提速时间
+    let holdingTime = self.calculateHoldingTime(self.physical); //最高速度坚持时间
+    let xDistance = destinationX - self.startX;
+    let yDistance = destinationY - self.startY;
+    let xDistanceAbs = Math.abs(xDistance); //绝对值，用于检测是否到达终点
+    let yDistanceAbs = Math.abs(yDistance); //绝对值，用于检测是否到达终点
+    let radian = Math.atan2(yDistance, xDistance); //到终点的弧度
+    let reach = false; //是否到达终点
+    let reachTime; //到达终点的时间
+    let reachSpeed; //到达终点时的速度，为后面停止做参数计算
 
     return new Promise((resolve, reject) =>{
       requestAnimationFrame(function update(){
-        let nowTime = new Date();
-        let offsetTime = nowTime - startTime;
+        let curtime = new Date();
+        let periodTime = curtime - startTime;
         let offsetX, offsetY, sumOffsetX, sumOffsetY;
 
-        if(!max){
-          if(offsetTime <= speedTime){
-            let speed = offsetTime / speedTime * self.maxSpeed
-            currentSpeed = Math.min(speed, self.maxSpeed);
-          }
+        if(!reach){
+          // 如果还未到达终点
+          if(periodTime <= speedUpTime){
+            currentSpeed = (periodTime / speedUpTime) * self.maxSpeed;
+          }else if(periodTime < (speedUpTime + holdingTime)){
+            currentSpeed = self.maxSpeed;
+          }else{
+            // 超过最高速度的坚持时间后每秒下降5个速度点
+            let speed = currentSpeed - (periodTime - speedUpTime - holdingTime) / 1000 * 5;
 
-          if(offsetTime >= (speedTime + keepTime)){
-            let speed = currentSpeed - (offsetTime - speedTime - keepTime) * 0.01;
-            currentSpeed = Math.max(speed, 0);
+            currentSpeed = Math.max(speed, 1); // 减到少于1以后就取1为速度
           }
         }else{
-          if(currentSpeed > 50){
-            currentSpeed = Math.max(currentSpeed - 20, 0);
-          }else if(currentSpeed > 20){
-            currentSpeed = Math.max(currentSpeed - 8, 0);
-          }else{
-            currentSpeed = Math.max(currentSpeed - 3, 0);
-          }
+          // 如果已到达终点0.5秒停下
+          let overPeriodTime = curtime - reachTime;
+          currentSpeed = reachSpeed - overPeriodTime / 500 * reachSpeed;
+          currentSpeed = Math.max(currentSpeed, 0);
         }
 
-
-        offsetX = Math.cos(angle) * currentSpeed;
-        offsetY = Math.sin(angle) * currentSpeed;
+        offsetX = Math.cos(radian) * self.calculateMeter(currentSpeed) / 60 * 10; //除以60是浏览器1秒大约刷新次数，乘以10是因为 10个单位为1米
+        offsetY = Math.sin(radian) * self.calculateMeter(currentSpeed) / 60 * 10;
 
         self.currentX += offsetX;
         self.currentY += offsetY;
@@ -101,12 +102,16 @@ class FootballPlayer extends Player{
         sumOffsetX = Math.abs(self.currentX - self.startX);
         sumOffsetY = Math.abs(self.currentY - self.startY);
 
-        if(xAbsDirection <= sumOffsetX && yAbsDirection <= sumOffsetY){
-          max = max || true;
+        if(xDistanceAbs <= sumOffsetX && yDistanceAbs <= sumOffsetY){
+          reach = reach || true;
+          reachTime = reachTime || new Date();
+          reachSpeed = reachSpeed || currentSpeed;
 
           if(currentSpeed === 0){
             self.startX = self.currentX;
             self.startY = self.currentY;
+
+            console.log(`period: ${new Date() - startTime}`)
 
             resolve();
           }else{
@@ -121,11 +126,11 @@ class FootballPlayer extends Player{
   calculateMeter(speed){
     return 3 + (speed - 1) / 98 * 9;
   }
-  calculateSpeedTime(explose){
-    return 4000 - (explose - 1) / 98 * 3000;
+  calculateSpeedUpTime(explosive){
+    return 4000 - (explosive - 1) / 98 * 3000;
   }
-  calculateKeepTime(energy){
-    return 10000 + (energy - 1) / 98 * 5000;
+  calculateHoldingTime(physical){
+    return 10000 + (physical - 1) / 98 * 5000;
   }
 }
 
